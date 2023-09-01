9a58e82f2b32fb572fe90fa2c1190e295c8cef5b
Aydan <aydan2903@gmail.com>
2023-09-01 13:19:46 +0400

    added items, error, loading page and app is ready

--- next03/app/[searchTerm]/page.tsx
+++ next03/app/[searchTerm]/page.tsx
@@ -1,14 +1,26 @@
 import getWikiResults from "../lib/getWikiResults"
-
+import Items from "./components/Items";
 type Props = {
     params:{
         searchTerm:string
     }
 }
 
-// export async function generateMetaData(){
-
-// }
+export async function generateMetaData({params: {searchTerm}}:Props): Promise<{ title: string; description: string }> {
+    const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
+    const data = await wikiData
+    const displayTerm = searchTerm.replaceAll("%20", " ")
+   if(!data?.query?.pages){
+    return{
+        title:`${displayTerm} not found`,
+        description: " "
+    }
+   }
+   return{
+    title:displayTerm,
+    description:` Search Results is ${displayTerm}`
+   }
+}
 
 export default async function SearchResults({ params: { searchTerm } }: Props) {
     const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
@@ -16,10 +28,10 @@ export default async function SearchResults({ params: { searchTerm } }: Props) {
     const results: Result[] | undefined = data?.query?.pages
 
     const content = (
-        <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
+        <main className="bg-slate-400 mx-auto max-w-3xl py-1 min-h-screen">
             {results
                 ? Object.values(results).map(result => {
-                    return <p key={result.pageid}  > {JSON.stringify(result)}</p>
+                    return <Items key={result.pageid} result={result}/>
                 })
                 : <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>
             }
